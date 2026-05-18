b3ec244e9540b912e6b70f1d49946e5366d07193
ctsawyer <ctsawyer@juno.com>
2026-05-17 20:45:32 -0500

    Update main.tf the second

--- gcp/terraform/bucket-public/main.tf
+++ gcp/terraform/bucket-public/main.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "uut" {
   name          = var.bucket_name_chris_test
   location      = var.bucket_location
   force_destroy = true
+  break this
 }
 resource "google_storage_bucket_iam_member" "uut" {
   bucket = google_storage_bucket.uut.name
